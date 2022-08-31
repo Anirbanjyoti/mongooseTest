4f11b622152a5c268ee4b1e63bbc5e64496de748
Anirbanjyoti <anirbanjyoti708@gmail.com>
2022-08-31 21:38:36 +0530

    Update one and get all student

--- routers/studentHandler.js
+++ routers/studentHandler.js
@@ -7,7 +7,24 @@ const studentSchema = require("../models/studentSchema");
 const Student = new mongoose.model("Student", studentSchema);
 
 // Get ALl The Student
-router.get("/", async (req, res) => {});
+router.get("/", async (req, res) => {
+  await Student.find({status:"active"}).select({
+    _id: 0,
+    _v: 0,
+    date: 0
+  }).exec((err, data)=>{
+    if (err) {
+      res.status(500).json({
+        error: "There was server side error",
+      });
+    } else {
+      res.status(200).json({
+        result: data,
+        message: "Student record inserted successfully",
+      });
+    }
+  })
+});
 
 // Get a single Student by id
 router.get("/:id", async (req, res) => {});
@@ -48,7 +65,27 @@ router.post("/all", async (req, res) => {
   });
 });
 //Put Student
-router.put("/:id", async (req, res) => {});
+router.put("/:id", async (req, res) => {
+  await Student.updateOne(
+    { _id: req.params.id },
+    {
+      $set: {
+        status: "inactive",
+      },
+    },
+    (err) => {
+      if (err) {
+        res.status(500).json({
+          error: "There was server side error",
+        });
+      } else {
+        res.status(200).json({
+          message: "Student record has been Updated successfully",
+        });
+      }
+    }
+  );
+});
 //Delete Student
 router.delete("/:id", async (req, res) => {});
 
