6ab06bb498dfb45fec87675cb66017fad15be92d
Anirbanjyoti <anirbanjyoti708@gmail.com>
2022-08-31 17:58:01 +0530

    insert many data at on time

--- routers/studentHandler.js
+++ routers/studentHandler.js
@@ -13,32 +13,40 @@ router.get("/", async (req, res) => {});
 router.get("/:id", async (req, res) => {});
 // POST/create a single Student
 router.post("/", async (req, res) => {
-  // res.send('Hello from create Student')
-  console.log(req.body);
-
   const newStudent = new Student(req.body);
-  await newStudent
-    .save((err)=>{
-      if(err){
-        res.status(500).json({
-          error: "There was server side error",
-        })
-      }else{
-        res.status(200).json({
-          message: "Student record inserted successfully",
-        })
-      }
-    })
-    // .then(() => {
-    //   res.status(201).send(newStudent);
-    // })
-    // .catch((e) => {
-    //   res.status(400).send(e);
-    // });
+  await newStudent.save((err) => {
+    if (err) {
+      res.status(500).json({
+        error: "There was server side error",
+      });
+    } else {
+      res.status(200).json({
+        message: "Student record inserted successfully",
+      });
+    }
+  });
+  // .then(() => {
+  //   res.status(201).send(newStudent);
+  // })
+  // .catch((e) => {
+  //   res.status(400).send(e);
+  // });
 });
 
 //POST/create multiple Student
-router.post("/all", async (req, res) => {});
+router.post("/all", async (req, res) => {
+  await Student.insertMany(req.body, (err) => {
+    if (err) {
+      res.status(500).json({
+        error: "There was server side error",
+      });
+    } else {
+      res.status(200).json({
+        message: "Student's All record inserted successfully",
+      });
+    }
+  });
+});
 //Put Student
 router.put("/:id", async (req, res) => {});
 //Delete Student
